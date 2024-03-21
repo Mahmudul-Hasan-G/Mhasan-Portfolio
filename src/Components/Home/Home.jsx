0e95b42aef5369c9bdf152c0ebda1602054e17b4
Mahmudul-Hasan-G <shantohasan1983@gmail.com>
2024-03-21 15:46:28 +0100

    home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,9 +2,18 @@
 
 const Home = () => {
   return (
-    <div>
-      <h1>Hi I am a Web Developer and Jonior Data Analitics</h1>
-      <img src="/public/hasan.jpg" alt="" />
+    <div className="grid grid-cols-12 gap-4 content-center  bg-orange-50">
+    <img className="col-span-1" src="/public/developer.png" alt="" />
+    <div className="col-span-7 grid content-center ">
+    <h1 className="text-4xl mb-6">Hi, I am</h1>
+    <h1 className="text-8xl mb-6 font-bold">Mahmudul Hasan</h1>
+    <h1 className="text-4xl">Ready to engage with the team and company.Easy to
+                        connect.Adopting the situation with a smile.</h1>
+    </div>
+      <div className="col-span-4 grid content-center">
+      <img className="rounded-full" src="/public/hasan.jpg" alt="" />
+      </div>
+      
     </div>
   );
 };
