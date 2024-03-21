c7a6cd72e8c20ede73ca3e83f9a77837bcbd35a5
Mahmudul-Hasan-G <shantohasan1983@gmail.com>
2024-03-21 14:12:30 +0100

    home

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router-dom'
 import './App.css'
 import Footer from './Components/Footer/Footer'
 import Header from './Components/Header/Header'
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Header />
+      <Outlet />
       <Footer />
     </>
   )
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import hasan from "/public/hasan.jpg"
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Hi I am a Web Developer and Jonior Data Analitics</h1>
+      <img src="hasan" alt="" />
+    </div>
+  );
+};
+
+export default Home;
