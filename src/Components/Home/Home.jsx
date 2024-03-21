3331302ab75fcf454fc1781a15493199c02537ec
Mahmudul-Hasan-G <shantohasan1983@gmail.com>
2024-03-21 15:52:25 +0100

    home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,7 +2,8 @@
 
 const Home = () => {
   return (
-    <div className="grid grid-cols-12 gap-4 content-center  bg-orange-50">
+    <div style={{
+      backgroundImage: 'url("/public/header_bg.png")',  backgroundSize: 'cover'}} className="grid grid-cols-12 gap-4 content-center  bg-orange-50">
     <img className="col-span-1" src="/public/developer.png" alt="" />
     <div className="col-span-7 grid content-center ">
     <h1 className="text-4xl mb-6">Hi, I am</h1>
@@ -10,7 +11,7 @@ const Home = () => {
     <h1 className="text-4xl">Ready to engage with the team and company.Easy to
                         connect.Adopting the situation with a smile.</h1>
     </div>
-      <div className="col-span-4 grid content-center">
+      <div  className="col-span-4 grid content-center">
       <img className="rounded-full" src="/public/hasan.jpg" alt="" />
       </div>
       
