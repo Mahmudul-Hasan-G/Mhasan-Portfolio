d5fad23ebc8f09e131b6e5a07f90d6c6251eed53
Mahmudul-Hasan-G <shantohasan1983@gmail.com>
2024-03-22 14:52:05 +0100

    aboutMe

--- src/Components/Certifications/Certifications.jsx
+++ src/Components/Certifications/Certifications.jsx
@@ -2,9 +2,37 @@
 
 const Certifications = () => {
   return (
-    <div>
-      <h1>this ia /certifications page</h1>
+    <div className=" bg-orange-50">
+    
+<div className="grid grid-cols-2  mx-6 gap-6">
+<div>
+<div className="text-2xl text-center">
+    <h1 className="pt-6">WEB-DEVELOPER </h1><br />
+    <h1>
+DCI DIGITAL CAREER INSTITUTE GMBH, BERLIN, GERMANY</h1><br />
+<h1>
+2023-2024</h1><br />
+<h1>HTML, CSS, JavaScript, React, MongoDB,  REST API,
+FireBase, JWT etc</h1><br />
     </div>
+<img className="my-6" src="/public/DCI_E.jpg" alt="" />
+</div>
+      <div>
+      <div className="text-2xl text-center">
+    <h1 className="pt-6">WEB-DEVELOPER </h1><br />
+    <h1>
+DCI DIGITAL CAREER INSTITUTE GMBH, BERLIN, GERMANY</h1><br />
+<h1>
+2023-2024</h1><br />
+<h1>HTML, CSS, JavaScript, React, MongoDB,  REST API,
+FireBase, JWT etc</h1><br />
+    </div>
+    <img className="my-6" src="/public/Coursera_E.jpg" alt="" />
+      </div>
+      
+    </div>
+    </div>
+    
   );
 };
 
