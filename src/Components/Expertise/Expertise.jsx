9818bca7f14edd15c2b34bcf263c94cd3692ee46
Mahmudul-Hasan-G <shantohasan1983@gmail.com>
2024-04-11 16:24:07 +0200

    expartis

--- src/Components/Expertise/Expertise.jsx
+++ src/Components/Expertise/Expertise.jsx
@@ -1,7 +1,42 @@
 import { SiReact } from "react-icons/si";
+import { TiHtml5 } from "react-icons/ti";
+import { FaCss3Alt } from "react-icons/fa6";
+import { IoLogoJavascript } from "react-icons/io5";
 
 const Expertise = () => {
   return (
+    <div className="grid grid-cols-5 gap-6  bg-orange-50 justify-items-center">
+
+      <div className="p-12 bg-orange-50">
+    <div className="grid grid-rows-6 gap-2 justify-items-center shadow-md w-36">
+    <div className="h-16 w-16 row-span-2">
+    <h1 className="text-orange-600 text-6xl shadow-md"><TiHtml5 /></h1>
+    </div>  
+    <h1 className="text-2xl font-bold row-span-1">HTML:5</h1>
+    <h1 className="row-span-3 text-center">Excellent experience of learning and working. </h1>
+    </div>
+    </div>
+
+    <div className="p-12 bg-orange-50">
+    <div className="grid grid-rows-6 gap-2 justify-items-center shadow-md w-36">
+    <div className="h-16 w-16 row-span-2">
+    <h1 className="text-indigo-600 text-6xl shadow-md"><FaCss3Alt /></h1>
+    </div>  
+    <h1 className="text-2xl font-bold row-span-1">CSS</h1>
+    <h1 className="row-span-3 text-center">Excellent experience of learning and working. </h1>
+    </div>
+    </div>
+
+    <div className="p-12 bg-orange-50">
+    <div className="grid grid-rows-6 gap-2 justify-items-center shadow-md w-36">
+    <div className="h-16 w-16 row-span-2">
+    <h1 className="text-amber-500 text-6xl shadow-md"><IoLogoJavascript /></h1>
+    </div>  
+    <h1 className="text-2xl font-bold row-span-1">JavaScript</h1>
+    <h1 className="row-span-3 text-center">Excellent experience of learning and working. </h1>
+    </div>
+    </div>
+
     <div className="p-12 bg-orange-50">
     <div className="grid grid-rows-6 gap-2 justify-items-center shadow-md w-36">
     <div className="h-16 w-16 row-span-2">
@@ -10,6 +45,18 @@ const Expertise = () => {
     <h1 className="text-2xl font-bold row-span-1">React</h1>
     <h1 className="row-span-3 text-center">Excellent experience of learning and working. </h1>
     </div>
+    </div>
+
+    <div className="p-12 bg-orange-50">
+    <div className="grid grid-rows-6 gap-2 justify-items-center shadow-md w-36">
+    <div className="h-16 w-16 row-span-2">
+    <h1 className="text-cyan-400 text-6xl shadow-md"><SiReact /></h1>
+    </div>  
+    <h1 className="text-2xl font-bold row-span-1">React</h1>
+    <h1 className="row-span-3 text-center">Excellent experience of learning and working. </h1>
+    </div>
+    </div>
+
     </div>
   );
 };
