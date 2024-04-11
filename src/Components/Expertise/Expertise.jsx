06d210d68669f8b25670662b79c630e233a8998e
Mahmudul-Hasan-G <shantohasan1983@gmail.com>
2024-04-11 16:35:22 +0200

    expartis

--- src/Components/Expertise/Expertise.jsx
+++ src/Components/Expertise/Expertise.jsx
@@ -2,6 +2,7 @@ import { SiReact } from "react-icons/si";
 import { TiHtml5 } from "react-icons/ti";
 import { FaCss3Alt } from "react-icons/fa6";
 import { IoLogoJavascript } from "react-icons/io5";
+import { SiPhp } from "react-icons/si";
 
 const Expertise = () => {
   return (
@@ -50,9 +51,49 @@ const Expertise = () => {
     <div className="p-12 bg-orange-50">
     <div className="grid grid-rows-6 gap-2 justify-items-center shadow-md w-36">
     <div className="h-16 w-16 row-span-2">
-    <h1 className="text-cyan-400 text-6xl shadow-md"><SiReact /></h1>
+    <h1 className="text-indigo-600 text-6xl shadow-md"><SiPhp /></h1>
     </div>  
-    <h1 className="text-2xl font-bold row-span-1">React</h1>
+    <h1 className="text-2xl font-bold row-span-1">PHP</h1>
+    <h1 className="row-span-3 text-center">Excellent experience of learning and working. </h1>
+    </div>
+    </div>
+
+    <div className="p-12 bg-orange-50">
+    <div className="grid grid-rows-6 gap-2 justify-items-center shadow-md w-36">
+    <div className="h-16 w-16 row-span-2">
+    <h1 className="text-indigo-600 text-6xl shadow-md"><SiPhp /></h1>
+    </div>  
+    <h1 className="text-2xl font-bold row-span-1">PHP</h1>
+    <h1 className="row-span-3 text-center">Excellent experience of learning and working. </h1>
+    </div>
+    </div>
+
+    <div className="p-12 bg-orange-50">
+    <div className="grid grid-rows-6 gap-2 justify-items-center shadow-md w-36">
+    <div className="h-16 w-16 row-span-2">
+    <h1 className="text-indigo-600 text-6xl shadow-md"><SiPhp /></h1>
+    </div>  
+    <h1 className="text-2xl font-bold row-span-1">PHP</h1>
+    <h1 className="row-span-3 text-center">Excellent experience of learning and working. </h1>
+    </div>
+    </div>
+
+    <div className="p-12 bg-orange-50">
+    <div className="grid grid-rows-6 gap-2 justify-items-center shadow-md w-36">
+    <div className="h-16 w-16 row-span-2">
+    <h1 className="text-indigo-600 text-6xl shadow-md"><SiPhp /></h1>
+    </div>  
+    <h1 className="text-2xl font-bold row-span-1">PHP</h1>
+    <h1 className="row-span-3 text-center">Excellent experience of learning and working. </h1>
+    </div>
+    </div>
+
+    <div className="p-12 bg-orange-50">
+    <div className="grid grid-rows-6 gap-2 justify-items-center shadow-md w-36">
+    <div className="h-16 w-16 row-span-2">
+    <h1 className="text-indigo-600 text-6xl shadow-md"><SiPhp /></h1>
+    </div>  
+    <h1 className="text-2xl font-bold row-span-1">PHP</h1>
     <h1 className="row-span-3 text-center">Excellent experience of learning and working. </h1>
     </div>
     </div>
