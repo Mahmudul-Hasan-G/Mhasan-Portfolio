066e5045585c41cee74a8a1442705af92c0761e4
Mahmudul-Hasan-G <shantohasan1983@gmail.com>
2024-04-11 20:57:08 +0200

    expartis

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,17 @@
 import react from 'react';
 
+const pdf_File = "http://localhost:5173/cv.pdf";
+
 const Home = () => {
+  const downloadFile = (url) =>{
+    
+    const aTag = document.createElement('a');
+    aTag.href = url;
+    aTag.setAttribute('download', 'cv.pdf' );
+    document.body.appendChild(aTag);
+    aTag.click();
+    aTag.remove();
+  }
   return (
     <div style={{
       backgroundImage: 'url("header_bg.png")',  backgroundSize: 'cover'}} className="grid grid-cols-12 gap-4 content-center  bg-orange-50">
@@ -10,7 +21,7 @@ const Home = () => {
     <h1 className="text-8xl mb-6 font-bold">Mahmudul Hasan</h1>
     <h1 className="text-4xl mb-12">Ready to engage with the team and company.Easy to
                         connect.Adopting the situation with a smile.</h1>
-                        <a className="btn bg-orange-500 h-12 w-44 text-xl font-bold">Download CV</a>
+                        <button onClick={() => {downloadFile(pdf_File)}}><a className="btn bg-orange-500 h-12 w-44 text-xl font-bold">Download CV</a></button>
     </div>
       <div  className="col-span-4 grid content-center">
       <img className="rounded-full" src="hasan.jpg" alt="" />
